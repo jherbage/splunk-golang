53579c296aa2b6f2635a2dee46a6e6760c61ebfe
Jason Herbage <jherbage@splunk.com>
2019-12-19 10:27:52 +0000

    wait added

--- search.go
+++ search.go
@@ -4,13 +4,18 @@ import (
 	"encoding/xml"
 	"fmt"
 	"net/url"
+	"time"
 )
 
-func (conn SplunkConnection) Search(search string) (string, error) {
+func (conn SplunkConnection) Search(search string, timeout int) (string, error) {
 	data := make(url.Values)
 	data.Add("search", search)
 	response, err := conn.httpPost(fmt.Sprintf("%s/services/search/jobs", conn.BaseURL), &data)
 	var searchResponse SearchSubmitResult
 	xml.Unmarshal([]byte(response), &searchResponse)
+	// We will wait until timeout before giving up on the search
+	for expired := time.Now().Add(time.Duration(timeout) * time.Second); expired.Sub(time.Now()) > 0; time.Sleep(5 * time.Second) {
+		fmt.Println("We waited")
+	}
 	return searchResponse.Sid, err
 }
